f5198ce8ba064bbc8fa9b9166b7d6e142ac54d3c
sarangp323 <sarangp323@gmail.com>
2021-08-11 18:17:18 +0530

    first commit

---
+++ counting_freq.py
@@ -0,0 +1,23 @@
+s = list(map(int, input().split(" ")))
+freq = {}
+for i in s:
+    if i in freq:
+        freq[i]+=1
+    else:
+        freq[i]=1
+for k,v in freq.items():
+    print(k,v)
+
+counter = {}
+for i in s:
+    counter[i] = counter.get(0,1) +1
+print(counter)
+
+f=[]
+for v in sorted(counter.items()):
+    for i in range(v):
+        f.append(k)
+print(f)
+        
+        
+    
