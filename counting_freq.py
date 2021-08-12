cf7bcd7c281c78aeb3a0e490892d293efe52e0ff
sarangp323 <sarangp323.com>
2021-08-12 17:46:06 +0530

    using clone

--- counting_freq.py
+++ counting_freq.py
@@ -14,14 +14,14 @@ for k,v in freq.items():
     print(k,v)
 
 # using short way
-counter = {}
+counter = {} #creating dict
 for i in s:
     counter[i] = counter.get(0,1) +1
 print(counter)
 
 print("Using Counter")
 f=[]
-for v in sorted(counter.items()):
+for v in sorted(counter.items()): #using counter 
     for i in range(v):
         f.append(k)
 print(f)
