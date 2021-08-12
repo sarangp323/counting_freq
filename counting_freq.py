acba7f9f093a7ba80102a9e208afee0b117f9b5f
sarangp323 <sarangp323@gmail.com>
2021-08-12 11:06:32 +0530

    Editing with branch

--- counting_freq.py
+++ counting_freq.py
@@ -1,6 +1,8 @@
 print("counting freq. of element in list")
-print(" Different Ways:)
+print(" Different Ways:")
+print("Taking list as input:")
 s = list(map(int, input().split(" ")))
+print("Using Dictionary")
 freq = {}
 for i in s:
     if i in freq:
@@ -15,6 +17,7 @@ for i in s:
     counter[i] = counter.get(0,1) +1
 print(counter)
 
+print("Using Counter")
 f=[]
 for v in sorted(counter.items()):
     for i in range(v):
