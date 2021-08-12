93809c7d20c5e60498bced93493461016e999e02
sarangp323 <sarangp323@gmail.com>
2021-08-12 16:59:09 +0530

    tr

--- counting_freq.py
+++ counting_freq.py
@@ -1,20 +1,25 @@
 print("counting freq. of element in list")
-print(" Different Ways:)
+print(" Different Ways:")
+print("Taking list as input:")
 s = list(map(int, input().split(" ")))
+print("Using Dictionary")
+# using traditional way
 freq = {}
-for i in s:
-    if i in freq:
-        freq[i]+=1
+for i in s: #loop
+    if i in freq: #checking element in dict
+        freq[i]+=1 # if yes count +=1
     else:
         freq[i]=1
 for k,v in freq.items():
     print(k,v)
 
+# using short way
 counter = {}
 for i in s:
     counter[i] = counter.get(0,1) +1
 print(counter)
 
+print("Using Counter")
 f=[]
 for v in sorted(counter.items()):
     for i in range(v):
